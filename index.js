a3511cf21df651b08bf67c4b381efbaf836e6632
Pajar <92008208+Fjnesaa96@users.noreply.github.com>
2025-09-28 05:16:58 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-pink-100">
-      <h1 className="text-4xl font-bold text-pink-700">Undangan Pernikahan</h1>
-      <p className="mt-4 text-lg">Yudis ❤️ Hana</p>
-      <p className="mt-2">Sabtu, 20 Desember 2025</p>
-    </div>
-  );
-}
